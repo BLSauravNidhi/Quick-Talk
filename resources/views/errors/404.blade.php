41a3198517eeaf77bd16d2da5a72e7691d69cbf6
saurav <sauravkumarverma92@gmail.com>
2026-09-09 11:57:31 +0530

    Change(page): Add custom 404 page design.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/components.css'])
+    <title>404 | Not Found</title>
+</head>
+<body>
+    <div class="w-screen h-screen flex items-center justify-center">
+        <!-- component -->
+        <div class=" w-full flex flex-col items-center justify-center gap-6">
+            <div class=" w-40 flex justify-center">
+                <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+            </div>
+            <h3 class="poppins text-2xl font-bold">Opps! Something went wrong.</h3>
+        </div>
+    </div>
+</body>
+</html>
